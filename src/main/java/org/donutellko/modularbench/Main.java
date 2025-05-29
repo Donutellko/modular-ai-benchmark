6e71e700e35016d35a77e5f335c76c5bddbd87e1
Donat Shergalis <donutellko@gmail.com>
2025-05-30 00:55:10 +0200

    add basic CLI parsing

--- src/main/java/org/donutellko/modularbench/Main.java
+++ src/main/java/org/donutellko/modularbench/Main.java
@@ -1,6 +1,50 @@
 package org.donutellko.modularbench;
 
+import org.apache.commons.cli.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        Options options = new Options();
+
+        OptionGroup commands = new OptionGroup();
+        commands.addOption(new Option("bench", false, "Run the benchmark"));
+        commands.setRequired(true);
+        options.addOptionGroup(commands);
+
+        options.addOption(Option.builder("config")
+                .hasArg()
+                .argName("file.yaml")
+                .desc("Path to the configuration YAML file")
+                .required()
+                .build());
+
+        options.addOption(Option.builder("tasks")
+                .hasArgs()
+                .argName("file1.yaml,file2.yaml,...")
+                .desc("Comma-separated list of task YAML file paths")
+                .required()
+                .build());
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine line = parser.parse(options, args);
+
+            if (line.hasOption("bench")) {
+                String configPath = line.getOptionValue("config");
+                String[] taskPaths = line.getOptionValues("tasks");
+
+                System.out.println("Running benchmark with config: " + configPath);
+                System.out.println("Task files: ");
+                for (String task : taskPaths) {
+                    System.out.println(" - " + task);
+                }
+            }
+        } catch (ParseException exp) {
+            System.err.println("Parsing failed. Reason: " + exp.getMessage());
+        }
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar modularbench.jar", options, true);
     }
 }
